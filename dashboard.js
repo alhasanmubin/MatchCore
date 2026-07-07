a5bf474f920821b530372d5b6631cd00a1211cee
alhasanmubin <alhasanmubin@gmail.com>
2026-07-08 03:43:49 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2449,3 +2449,12 @@ if ("serviceWorker" in navigator) {
         });
     });
 }
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js").then((reg) => {
+            console.log("Service Worker registered: ", reg.scope);
+        }).catch((err) => {
+            console.error("Service Worker registration failed: ", err);
+        });
+    });
+}
